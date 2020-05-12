62ce7af800b53d571006918a317b4839954c6759
manuel <mabril@udesa.edu.ar>
2020-05-12 19:18:49 -0300

    ERROR DE , se ve mal pero anda al menos

--- proyecto-final-prog2/routes/resultados.js
+++ proyecto-final-prog2/routes/resultados.js
@@ -8,7 +8,7 @@ const controller = require('../controllers/resultadosController')
 //Rutas
 
 //localhost:3000/resultados
-router.get('/'. controller.index)
+router.get('/', controller.index)
 
 //localhost:3000/resultados/pelicula/:id
 router.get('/pelicula/:id', controller.pelicula)
