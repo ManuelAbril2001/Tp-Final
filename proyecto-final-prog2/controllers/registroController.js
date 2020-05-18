6b67a8f81d85c430408c573774a3a7be83d7cc8d
manuel <mabril@udesa.edu.ar>
2020-05-18 18:20:06 -0300

    uikit 2

--- proyecto-final-prog2/controllers/registroController.js
+++ proyecto-final-prog2/controllers/registroController.js
@@ -1,6 +1,14 @@
+const DB = require('../database/models');
+const OP = DB.Sequelize.Op;
+
+
 module.exports = {
     index: function(req,res){
         res.render('registro')
+
+    },
+    validacion: function(req,res){
+        res.render('index')
 
     }
 }
